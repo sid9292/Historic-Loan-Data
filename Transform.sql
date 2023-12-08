b78131eabe58ef9c4c363f148a588682cbe6fc35
Sid Cherif <145723788+sid9292@users.noreply.github.com>
2023-12-08 00:21:04 -0500

    Update Transform.sql

--- Transform.sql
+++ Transform.sql
@@ -1,21 +1,39 @@
-DELETE FROM [dbo].[hmda_2017_nationwide_all-records_labels]
-WHERE applicant_race_name_1 IS NULL
-   OR applicant_race_1 IS NULL
-   OR applicant_race_name_2 IS NULL
-   OR applicant_race_2 IS NULL
-   OR applicant_race_name_3 IS NULL
-   OR applicant_race_3 IS NULL
-   OR applicant_race_name_4 IS NULL
-   OR applicant_race_4 IS NULL
-   OR applicant_race_name_5 IS NULL
-   OR applicant_race_5 IS NULL
-   OR co_applicant_race_name_1 IS NULL
-   OR co_applicant_race_1 IS NULL
-   OR co_applicant_race_name_2 IS NULL
-   OR co_applicant_race_2 IS NULL
-   OR co_applicant_race_name_3 IS NULL
-   OR co_applicant_race_3 IS NULL
-   OR co_applicant_race_name_4 IS NULL
-   OR co_applicant_race_4 IS NULL
-   OR co_applicant_race_name_5 IS NULL
-   OR co_applicant_race_5 IS NULL;
+CREATE TABLE AgencyDimension (
+    agency_id INT PRIMARY KEY,
+    agency_name VARCHAR(255),
+    agency_abbr VARCHAR(10),
+    agency_code INT
+);
+
+CREATE TABLE LoanTypeDimension (
+    loan_type_id INT PRIMARY KEY,
+    loan_type_name VARCHAR(255)
+);
+
+
+CREATE TABLE PropertyTypeDimension (
+    property_type_id INT PRIMARY KEY,
+    property_type_name VARCHAR(255)
+);
+
+
+CREATE TABLE LoanPurposeDimension (
+    loan_purpose_id INT PRIMARY KEY,
+    loan_purpose_name VARCHAR(255)
+);
+
+-- Create Owner Occupancy Dimension Table
+CREATE TABLE OwnerOccupancyDimension (
+    owner_occupancy_id INT PRIMARY KEY,
+    owner_occupancy_name VARCHAR(255)
+);
+
+CREATE TABLE PreapprovalDimension (
+    preapproval_id INT PRIMARY KEY,
+    preapproval_name VARCHAR(255)
+);
+
+
+CREATE TABLE ActionTakenDimension (
+    action_taken_id INT PRIMARY KEY,
+    action_taken_name VARCHAR(255)
