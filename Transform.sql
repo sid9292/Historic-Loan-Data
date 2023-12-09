153279d2b3f44e71fe7b7f8a4d94b01f9c412abd
Sid Cherif <145723788+sid9292@users.noreply.github.com>
2023-12-09 13:21:31 -0500

    Update Transform.sql

--- Transform.sql
+++ Transform.sql
@@ -1,3 +1,27 @@
+import pandas as pd
+
+csv_file_path = 'C:/Users/s.larbicherif/Desktop/home_mortgage.csv'
+
+columns_to_extract = [
+    
+    'loan_amount_000s',
+    'applicant_income_000s',
+    'population',
+    'state_name',
+    'minority_population',
+    'applicant_race_name_1',
+    'hud_median_family_income',
+    'number_of_owner_occupied_units',
+    'applicant_sex_name'
+]
+
+df = pd.read_csv(csv_file_path, nrows=1000)
+    
+if all(column in df.columns for column in columns_to_extract):
+    # Extract the specified columns
+    selected_columns = df[columns_to_extract]
+    selected_columns.to_csv('extracted_data.csv', index=False)
+    
 CREATE TABLE AgencyDimension (
     agency_id INT PRIMARY KEY,
     agency_name VARCHAR(255),
@@ -22,7 +46,7 @@ CREATE TABLE LoanPurposeDimension (
     loan_purpose_name VARCHAR(255)
 );
 
--- Create Owner Occupancy Dimension Table
+
 CREATE TABLE OwnerOccupancyDimension (
     owner_occupancy_id INT PRIMARY KEY,
     owner_occupancy_name VARCHAR(255)
